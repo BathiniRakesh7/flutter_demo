430b54e341655f1a8af9bbac70df830241f92fcc
Rakesh Bathini <rakeshbathini07@gmail.com>
2023-05-17 22:52:46 +0530

    added container and column

--- lib/main.dart
+++ lib/main.dart
@@ -9,28 +9,28 @@ void main() {
             title: const Center(
               child: Text('Flutter'),
             ),
-            backgroundColor: const Color.fromARGB(255, 37, 13, 189),
+            backgroundColor: const Color.fromARGB(255, 92, 189, 13),
           ),
           body: SafeArea(
               child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Container(
-                color: Colors.lightBlue[100],
-                width: 200,
+                color: Colors.indigo,
+                width: 400,
                 height: 200,
-                padding: const EdgeInsets.all(10),
-                margin: const EdgeInsets.fromLTRB(550, 0, 0, 0),
+                padding: const EdgeInsets.all(20),
+                margin: const EdgeInsets.fromLTRB(300, 0, 0, 0),
                 child: const Image(
                     image: NetworkImage(
                         'https://1.bp.blogspot.com/-9oEdNse-Jpg/XqGy4q33HII/AAAAAAAAAq0/b_1A9VJPdrAJANfxpBIlWHUqcaMC0-LfACLcBGAsYHQ/w1200-h630-p-k-no-nu/1.jpg')),
               ),
               Container(
-                color: Colors.greenAccent,
+                color: Colors.pink[700],
                 width: 150,
                 height: 150,
                 padding: const EdgeInsets.all(5),
-                margin: const EdgeInsets.fromLTRB(550, 0, 0, 0),
+                margin: const EdgeInsets.fromLTRB(300, 0, 0, 0),
                 child: const Image(
                     image: NetworkImage(
                         'https://tse3.mm.bing.net/th?id=OIP.hd3tZTnUwT8tGEgqRcQtAwHaEK&pid=Api&P=0&h=180')),
@@ -40,7 +40,7 @@ void main() {
                 width: 150,
                 height: 150,
                 padding: const EdgeInsets.all(5),
-                margin: const EdgeInsets.fromLTRB(550, 0, 0, 0),
+                margin: const EdgeInsets.fromLTRB(300, 0, 0, 0),
                 child: const Image(
                     image: NetworkImage(
                         'https://1.bp.blogspot.com/-g4tcgj4zNaw/XatRyQkZ-JI/AAAAAAAAIWk/BKR6OHbKU9A74CiCGXVyZ8AfLIqwQF4kACNcBGAsYHQ/w1200-h630-p-k-no-nu/cpp.png')),
