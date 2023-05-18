6b487d27f4fd7454d829674596d2bb8cef559019
Rakesh Bathini <rakeshbathini07@gmail.com>
2023-05-18 16:54:36 +0530

    added Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -20,36 +20,58 @@ class MyWidget extends StatelessWidget {
           ),
           backgroundColor: const Color.fromARGB(255, 250, 146, 37),
         ),
+        drawer: Drawer(
+          child: ListView(
+            children: const [
+              UserAccountsDrawerHeader(
+                accountName: Text('company'),
+                accountEmail: Text('cerone@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  child: ClipOval(
+                    child: Image(
+                      image: NetworkImage(
+                        'https://tse2.mm.bing.net/th?id=OIP.cnmJQGN_3J6Zm0Zwi0fAVAAAAA&pid=Api&P=0&h=180',
+                      ),
+                      width: 50,
+                      height: 50,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
         body: SafeArea(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                  width: 400,
-                  height: 200,
-                  child: const Image(
-                      image: NetworkImage(
-                          'https://1.bp.blogspot.com/-9oEdNse-Jpg/XqGy4q33HII/AAAAAAAAAq0/b_1A9VJPdrAJANfxpBIlWHUqcaMC0-LfACLcBGAsYHQ/w1200-h630-p-k-no-nu/1.jpg')),
-                ),
-                Container(
-                  width: 150,
-                  height: 100,
-                  padding: const EdgeInsets.all(5),
-                  child: const Image(
-                      image: NetworkImage(
-                          'https://tse3.mm.bing.net/th?id=OIP.hd3tZTnUwT8tGEgqRcQtAwHaEK&pid=Api&P=0&h=180')),
-                ),
-                Container(
-                  width: 150,
-                  height: 100,
-                  padding: const EdgeInsets.all(3),
-                  child: const Image(
-                      image: NetworkImage(
-                          'https://1.bp.blogspot.com/-g4tcgj4zNaw/XatRyQkZ-JI/AAAAAAAAIWk/BKR6OHbKU9A74CiCGXVyZ8AfLIqwQF4kACNcBGAsYHQ/w1200-h630-p-k-no-nu/cpp.png')),
-                ),
-              ],
-            )),
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
+              width: 400,
+              height: 200,
+              child: Image(
+                  image: NetworkImage(
+                      'https://1.bp.blogspot.com/-9oEdNse-Jpg/XqGy4q33HII/AAAAAAAAAq0/b_1A9VJPdrAJANfxpBIlWHUqcaMC0-LfACLcBGAsYHQ/w1200-h630-p-k-no-nu/1.jpg')),
+            ),
+            Container(
+              width: 150,
+              height: 100,
+              padding: const EdgeInsets.all(5),
+              child: const Image(
+                  image: NetworkImage(
+                      'https://tse3.mm.bing.net/th?id=OIP.hd3tZTnUwT8tGEgqRcQtAwHaEK&pid=Api&P=0&h=180')),
+            ),
+            Container(
+              width: 150,
+              height: 100,
+              padding: const EdgeInsets.all(3),
+              child: const Image(
+                  image: NetworkImage(
+                      'https://1.bp.blogspot.com/-g4tcgj4zNaw/XatRyQkZ-JI/AAAAAAAAIWk/BKR6OHbKU9A74CiCGXVyZ8AfLIqwQF4kACNcBGAsYHQ/w1200-h630-p-k-no-nu/cpp.png')),
+            ),
+          ],
+        )),
         bottomNavigationBar: CurvedNavigationBar(
           backgroundColor: Colors.deepPurpleAccent,
           color: Colors.amber,
