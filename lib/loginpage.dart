ba3773f047a403907020cf7c08d5413afa24e42c
Rakesh Bathini <rakeshbathini07@gmail.com>
2023-05-19 22:32:17 +0530

    login Button updated

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_demo/main.dart';
 
 void main() {
-  runApp(const LoginPage());
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'login',
+      home: LoginPage(),
+    ),
+  );
 }
 
 class LoginPage extends StatelessWidget {
@@ -10,81 +16,88 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.grey,
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                const SizedBox(height: 50),
-                const Icon(
-                  Icons.lock,
-                  size: 70,
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+              const Icon(
+                Icons.lock,
+                size: 70,
+              ),
+              const SizedBox(height: 40),
+              const Text(
+                'WelCome To My App',
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 25,
                 ),
-                const SizedBox(height: 40),
-                const Text(
-                  'WelCome To My App',
-                  style: TextStyle(
-                    color: Colors.black54,
-                    fontSize: 25,
-                  ),
+              ),
+              const SizedBox(height: 25),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 100),
+                child: TextField(
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black54),
+                      ),
+                      fillColor: Colors.white70,
+                      filled: true,
+                      hintText: 'Email'),
                 ),
-                const SizedBox(height: 25),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 100),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black54),
-                        ),
-                        fillColor: Colors.white70,
-                        filled: true,
-                        hintText: 'Email'),
-                  ),
+              ),
+              const SizedBox(height: 10),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 100),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black54),
+                      ),
+                      fillColor: Colors.white70,
+                      filled: true,
+                      hintText: 'Password'),
                 ),
-                const SizedBox(height: 10),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 100),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black54),
-                        ),
-                        fillColor: Colors.white70,
-                        filled: true,
-                        hintText: 'Password'),
-                  ),
+              ),
+              const SizedBox(height: 10),
+              ConstrainedBox(
+                constraints:
+                    const BoxConstraints.tightFor(width: 120, height: 40),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurple),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const MyWidget()));
+                  },
+                  child: const Text('Login'),
                 ),
-                const SizedBox(height: 10),
-                ConstrainedBox(
-                  constraints:
-                      const BoxConstraints.tightFor(width: 120, height: 40),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.deepPurple),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const MyWidget()));
-                    },
-                    child: const Text('Login'),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
+
+//
+// floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+// floatingActionButton: FloatingActionButton(
+// onPressed: () {
+// Navigator.push(context,
+// MaterialPageRoute(builder: (context) => const MyWidget()));
+// },
+// child: const Icon(Icons.send),
+// ),
