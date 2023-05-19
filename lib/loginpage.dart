84b23cead8c95697bdf76784063ac2c5a5e515db
Rakesh Bathini <rakeshbathini07@gmail.com>
2023-05-19 21:42:47 +0530

    added login Button

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/main.dart';
 
 void main() {
   runApp(const LoginPage());
@@ -9,48 +10,46 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.grey,
         body: SafeArea(
           child: Center(
             child: Column(
-              //mainAxisAlignment: MainAxisAlignment.center,
-              //crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SizedBox(height: 50),
-                Icon(
+                const SizedBox(height: 50),
+                const Icon(
                   Icons.lock,
                   size: 70,
                 ),
-                SizedBox(height: 40),
-                Text(
-                    'WelCome To My App',
-                style: TextStyle(
-                  color: Colors.black54,
-                  fontSize: 25,
-                ),),
-                SizedBox(height: 25),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal:100),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black54),
-                    ),
-                      fillColor: Colors.white70,
-                      filled: true,
-                      hintText: 'Email'
+                const SizedBox(height: 40),
+                const Text(
+                  'WelCome To My App',
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 25,
                   ),
                 ),
+                const SizedBox(height: 25),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 100),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black54),
+                        ),
+                        fillColor: Colors.white70,
+                        filled: true,
+                        hintText: 'Email'),
+                  ),
                 ),
-                SizedBox(height: 10),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal:100),
+                const SizedBox(height: 10),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 100),
                   child: TextField(
                     obscureText: true,
                     decoration: InputDecoration(
@@ -62,8 +61,23 @@ class LoginPage extends StatelessWidget {
                         ),
                         fillColor: Colors.white70,
                         filled: true,
-                        hintText: 'Password'
-                    ),
+                        hintText: 'Password'),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                ConstrainedBox(
+                  constraints:
+                      const BoxConstraints.tightFor(width: 120, height: 40),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepPurple),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const MyWidget()));
+                    },
+                    child: const Text('Login'),
                   ),
                 ),
               ],
